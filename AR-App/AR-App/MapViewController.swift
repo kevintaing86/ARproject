3f22c5e7688ebb409afd034d99af4ad898280fd0
kevintaing86 <kevin.taing@ttu.edu>
2017-09-11 22:45:06 -0500

    Added mapview and set coordinates to TTU

--- AR-App/AR-App/MapViewController.swift
+++ AR-App/AR-App/MapViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let location = CLLocation(latitude: 33.5843, longitude: -101.8783)
+        let regionRadius: CLLocationDistance = 1000
+        
+        centerMapAtLocation(location: location, with: regionRadius)
 
+    }
+    
+    func centerMapAtLocation(location: CLLocation, with regionRadius: CLLocationDistance) {
+        let coordinateLocation = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        
+        self.mapView.setRegion(coordinateLocation, animated: true)
     }
 
 }
