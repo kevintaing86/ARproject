10f048611f6948b9387db1b141ff90a936890c05
kevintaing86 <kevin.taing@ttu.edu>
2017-09-24 00:28:43 -0500

    Created custom pin annotation and callout view

---
+++ AR-App/AR-App/AnnotationPopoverView.swift
@@ -0,0 +1,21 @@
+//
+//  AnnotationPopoverView.swift
+//  AR-App
+//
+//  Created by Kevin Taing on 9/23/17.
+//  Copyright © 2017 Kevin Taing. All rights reserved.
+//
+
+import UIKit
+
+class AnnotationPopoverView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
